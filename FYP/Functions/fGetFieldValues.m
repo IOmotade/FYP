9d06c1aa881f7f36c690994ebe06158685ed4e7e
IOmotade <taderomi@gmail.com>
2019-03-12 11:01:17 +0000

    General Update

--- FYP/Functions/fGetFieldValues.m
+++ FYP/Functions/fGetFieldValues.m
@@ -1,14 +1,11 @@
 function val = fGetFieldValues(structure, varargin)
 
-% field = char(field);
 [rowSize, colSize] = size(structure);
 
 scheme = length(varargin);
 for rowIdx = 1:rowSize
     for colIdx = 1:colSize
         val(rowIdx, colIdx, :, :) = fGetField(structure, rowIdx, colIdx, varargin, scheme);
-        %         val(rowIdx, colIdx, :, :) = getfield(structure, {rowIdx, colIdx}, field, 'value');
-        %         val(rowIdx, colIdx, :, :) = getfield(structure, {rowIdx, colIdx}, field);
     end
 end
 val = squeeze(val);
@@ -17,19 +14,22 @@ end
 function f = fGetField(structure, rowIdx, colIdx, var, scheme)
 if scheme ==1
     f = getfield(structure, {rowIdx, colIdx}, char(var{1}));
+    return
 end
 
 if scheme ==2
     f = getfield(structure, {rowIdx, colIdx}, char(var{1}), char(var{2}));
+    return
 end
 
 if scheme ==3
     f = getfield(structure, {rowIdx, colIdx}, char(var{1}), char(var{2}), char(var{3}));
+    return
 end
 
 if scheme ==4
     f = getfield(structure, {rowIdx, colIdx}, char(var{1}), char(var{2}), char(var{3}), char(var{4}));
-    
+    return
 end
 
 end
